5a1c762d831e04d09ce7a7be6fbb4446d90767e6
Sakeen Ahamed. M <shakeenahmed1010@gmail.com>
2025-03-08 15:33:31 +0530

    Update Header.jsx
    header changess

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,11 +1,5 @@
-import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList
-} from "@/components/ui/navigation-menu"
 import { Button } from "@/components/ui/button"
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { useState } from "react"
 import logo from "@/images/logo.jpg"
 import { AuthDialog } from "./Auth/AuthDialog"
@@ -33,79 +27,72 @@ export default function Header({ userInfo, setUserInfo, isLogged, setIsLogged })
                     <img className="size-8" src={logo} alt="logo icon"></img>
                     <h1 className="text-3xl text-black-500"><b>HOTELHUB</b></h1>
                 </div>
-                <NavigationMenu>
-                    <NavigationMenuList className="hidden md:flex gap-6">
-                        <NavigationMenuItem>
-                            <NavigationMenuLink
-                                className="text-gray-600 hover:text-gray-900"
-                                href="/"
-                            >
-                                <u><b>Home</b></u>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink
-                                className="text-gray-600 hover:text-gray-900"
-                                href="hotels"
-                            >
-                                <u><b>Hotels</b></u>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink
-                                className="text-gray-600 hover:text-gray-900"
-                                href="about"
-                            >
-                                <u><b>About</b></u>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
+                <div className="flex gap-8">
+                    <NavLink
+                        className="text-gray-600 hover:text-gray-900 font-bold"
+                        to="/"
+                    >                Home
+                    </NavLink>
+                    <NavLink
+                        className="text-gray-600 hover:text-gray-900 font-bold"
+                        to="/divisons"
+                    >
+                        Hotels
+                    </NavLink>
+                    <NavLink
+
+                        className="text-gray-600 hover:text-gray-900 font-bold"
+                        to="/about"
+                    >
+                        About
+                    </NavLink>
+                </div>
+
                 <div className="flex gap-4">
-                {isLogged ? (
-                    <ul className="flex items-center gap-8">
-                        <li title='Profile'>
-                            <NavLink to="/profile" className="">
-                                <Avatar className="size-8 bg-primary cursor-pointer select-none">
-                                    <AvatarFallback className="bg-primary font-semibold text-xl leading-8">
-                                        {userInfo?.name?.slice(0, 1).toUpperCase()}
-                                    </AvatarFallback>
-                                </Avatar>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <div className="hover:text-gray-300 cursor-pointer" title='Logout' onClick={logout}>
-                                <LogOut />
-                            </div>
-                        </li>
-                    </ul>
-                ) : (
-                    <ul className="flex items-center gap-8">
-                        <li>
-                            <Button
-                                onClick={() => {
-                                    setDialogOpen(true);
-                                    setAuth("login");
-                                }}
-                                variant='secondary'
-                                className="h-8 w-20 text-base"
-                            >
-                                Login
-                            </Button>
-                        </li>
-                        <li>
-                            <Button
-                                onClick={() => {
-                                    setDialogOpen(true);
-                                    setAuth("signup");
-                                }}
-                                className="h-8 w-20 text-base"
-                            >
-                                Sign up
-                            </Button>
-                        </li>
-                    </ul>
-                )}
+                    {isLogged ? (
+                        <ul className="flex items-center gap-8">
+                            <li title='Profile'>
+                                <NavLink to="/profile" className="">
+                                    <Avatar className="size-8 bg-primary cursor-pointer select-none">
+                                        <AvatarFallback className="bg-primary font-semibold text-xl leading-8">
+                                            {userInfo?.name?.slice(0, 1).toUpperCase()}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <div className="hover:text-gray-300 cursor-pointer" title='Logout' onClick={logout}>
+                                    <LogOut />
+                                </div>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className="flex items-center gap-8">
+                            <li>
+                                <Button
+                                    onClick={() => {
+                                        setDialogOpen(true);
+                                        setAuth("login");
+                                    }}
+                                    variant='secondary'
+                                    className="h-8 w-20 text-base"
+                                >
+                                    Login
+                                </Button>
+                            </li>
+                            <li>
+                                <Button
+                                    onClick={() => {
+                                        setDialogOpen(true);
+                                        setAuth("signup");
+                                    }}
+                                    className="h-8 w-20 text-base"
+                                >
+                                    Sign up
+                                </Button>
+                            </li>
+                        </ul>
+                    )}
                 </div>
             </div>
             <AuthDialog dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} auth={auth} setAuth={setAuth} setUserInfo={setUserInfo} setIsLogged={setIsLogged} />
